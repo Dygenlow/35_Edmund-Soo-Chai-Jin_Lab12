dc2ff7175f7c984bb451e591dcbb410bbf6cc5e4
Dygenlow <edmundsoo36@gmail.com>
2020-11-18 19:43:47 +0800

    Update 1.0

---
+++ 35_Edmund Soo Chai Jin_Lab12/Assets/Scripts/PlayerController35.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController35 : MonoBehaviour
+{
+    public GameObject CoinCollected;
+    public float speed;
+    public float rotateSpeed;
+
+    private int coinCount;
+    private int totalCoin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalCoin = GameObject.FindGameObjectsWithTag("Coin").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ForwardKey();
+        BackKey();
+        LeftRotate();
+        RightRotate();
+
+        if(coinCount == totalCoin)
+        {
+            print("You win!");
+            SceneManager.LoadScene("WinScene");
+        }
+
+        if(transform.position.y <= -5)
+        {
+            print("You lose");
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Coin")
+        {
+            coinCount++;
+
+            CoinCollected.GetComponent<Text>().text = "Coin Collected: " + coinCount;
+
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void ForwardKey()
+    {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+
+    private void BackKey()
+    {
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+    }
+
+    private void LeftRotate()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate (new Vector3(0, Time.deltaTime * -rotateSpeed, 0));
+        }
+
+    }
+
+    private void RightRotate()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * rotateSpeed, 0));
+        }
+    }
+}
---
+++ SceneDemo_35Edmund Soo Chai Jin/Assets/Scripts/PlayerController35.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController35 : MonoBehaviour
+{
+    public Text scoreText;
+
+    public float speed = 5.0f;
+    public float score = 10.0f;
+    int PowerUpLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ForwardKey();
+        BackKey();
+        LeftKey();
+        RightKey();
+
+        PowerUpLeft = GameObject.FindGameObjectsWithTag("PowerUp").Length;
+
+        print("PowerUps Left: " + PowerUpLeft);
+
+        scoreText.GetComponent<Text>().text = "Game Score: " + score;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            score--;
+
+            if(score <= 0)
+            {
+                score = 0.0f;
+            }
+        }
+
+        if(score == 0)
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+
+        if(PowerUpLeft == 0)
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PowerUp")
+        {
+            score++;
+
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.tag == "Enemy")
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+
+    private void ForwardKey()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+
+    private void BackKey()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+    }
+
+    private void LeftKey()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+    }
+
+    private void RightKey()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+    }
+}
